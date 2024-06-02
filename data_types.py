32cad64ca8f6dfe4001290fbea9c08aaee454b22
Incognito Sudo <gfranco.mg@gmail.com>
2024-06-01 19:10:23 -0700

    Add files via upload

---
+++ data_types.py
@@ -0,0 +1,14 @@
+"""
+Primitive Data types
+"""
+
+name = "Bob"
+age = 35
+cash = 100.25
+retired = False
+
+# Know data Type of variable by type(<Variable name>)
+print("Data type of Name:", type(name))
+print("Data type of age:", type(age))
+print("Data type of cash:", type(cash))
+print("Data type of retired:", type(retired))
