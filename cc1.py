debfa61899a1f3acd13d8f0e1529b85d439e5349
Incognito Sudo <gfranco.mg@gmail.com>
2024-06-01 19:09:42 -0700

    Create cc1.py

---
+++ cc1.py
@@ -0,0 +1,20 @@
+#Turn this line into a comment
+
+cities = ["Seattle", "Tacoma" , "Bellevue"]
+
+a = 1
+print(type(a))
+
+b = 8.0
+print("Data type of b:", type(b))
+
+c = "22"
+print("Data type of c:", type(c))
+
+d = a+b
+print(d)
+
+
+
+
+
